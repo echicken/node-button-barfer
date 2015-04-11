aaf7f3b6083eebbfecb8bd56cad84a02c4115a13
echicken <echicken@bbs.electronicchicken.com>
2015-04-11 03:11:48 -0400

    I added a tri-colour LED to the display board, so now there's a 'setFlairColour()' function. Possibly a few other changes that I've forgotten.

--- button.ino
+++ button.ino
@@ -6,7 +6,8 @@
 U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NO_ACK);
 
 // Your MAC address
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+byte mac[] = { 
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 
 // Your IP configuration
 IPAddress ip(192,168,1,11);
@@ -17,18 +18,23 @@ IPAddress subnet(255,255,255,0);
 IPAddress server(192,168,1,10);
 int port = 8000;
 
+// LED pins
+int redPin = 6;
+int greenPin = 5;
+int bluePin = 3;
+
 // Labels which map to values (below)
 char names[10][13] = {
- "Presses:",
- "/r/thebutton",
- "Greys:",
- "Purps:",
- "Blues:", 
- "Greens:",
- "Yellows:",
- "Oranges:",
- "Reds:",
- "Noobs:"
+  "Presses:",
+  "/r/thebutton",
+  "Greys:",
+  "Purps:",
+  "Blues:", 
+  "Greens:",
+  "Yellows:",
+  "Oranges:",
+  "Reds:",
+  "Noobs:"
 };
 
 // This will be populated with data from the server
@@ -57,31 +63,71 @@ void cycleDisplay() {
     u8g.setFont(u8g_font_04b_03);
     u8g.drawStr(85, 55, names[0]);
     u8g.drawStr(85, 63, values[0]);
-   
+
     // Draw the timer label and value
     u8g.drawStr(70, 5, names[1]);
     u8g.setFont(u8g_font_helvB24);
     u8g.drawStr(85, 40, values[1]);
- 
+
     // Display the 'active users' data
     u8g.setFont(u8g_font_04b_03);
     u8g.drawStr(2, 5, "Active users:");
     for(int i = 2; i < 10; i++) {
-     u8g.drawStr(2, (i*7), names[i]); 
-     u8g.drawStr(40, (i*7), values[i]);
+      u8g.drawStr(2, (i*7), names[i]); 
+      u8g.drawStr(40, (i*7), values[i]);
     }
 
-  } while(u8g.nextPage());
+  } 
+  while(u8g.nextPage());
 
+}
+
+void setFlairColour() {
+  int red;
+  int green;
+  int blue;
+  int c = atoi(values[1]);
+  if(c >= 52) {
+    red = 0x82;
+    green = 0x00;
+    blue = 0x80;
+  } else if(c >= 42) {
+    red = 0x00;
+    green = 0x83;
+    blue = 0xC7;
+  } else if(c >= 32) {
+    red = 0x02;
+    green = 0xBE;
+    blue = 0x01;
+  } else if(c >= 22) {
+    red = 0xE5;
+    green = 0xD9;
+    blue = 0x00;
+  } else if(c >= 12) {
+    red = 0xE5;
+    green = 0x95;
+    blue = 0x00;
+  } else if(c >= 1) {
+    red = 0xE5;
+    green = 0x00;
+    blue = 0x00;
+  }
+  analogWrite(redPin, red);
+  analogWrite(greenPin, green);
+  analogWrite(bluePin, blue);
 }
 
 void setup() {
 
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+  
   u8g.setColorIndex(1); // Set up a h'wite mono display, IIRC
   u8g.setFontPosTop(); // Not sure when or how often to call this
 
   // Configure our internets
-  Ethernet.begin(mac, ip, gateway, subnet); 
+  Ethernet.begin(mac, ip, gateway, subnet);
   delay(1000); // Ugh.
   ethernetClient.connect(server, port);
 
@@ -90,29 +136,29 @@ void setup() {
 void loop() {
 
   /*  We will naively assume that if any data is available from
-      the server, that a complete set is waiting to be read.
-      This is not great, but so far it hasn't borked on me.  */
+   the server, that a complete set is waiting to be read.
+   This is not great, but so far it hasn't borked on me.  */
   if(ethernetClient.available()) {
- 
+    
     /*  I could probably do all of these in one loop, but for
-        now I'll grab differently-sized values separately. */
+     now I'll grab differently-sized values separately. */
 
     // Read the participants count
     for(int n = 0; n < 10; n++) {
-     char c = ethernetClient.read();
-     // Stop at a newline
-     if(c == '\n') {
-      // Overwrite any remaining chars
-      while(n < 10) {
-        values[0][n] = 32;
-        n++;
+      char c = ethernetClient.read();
+      // Stop at a newline
+      if(c == '\n') {
+        // Overwrite any remaining chars
+        while(n < 10) {
+          values[0][n] = 32;
+          n++;
+        }
+        break;
       }
-      break;
-     }
-     // If we're still here, append the char to this value
-     values[0][n] = c;
+      // If we're still here, append the char to this value
+      values[0][n] = c;
     }
-    
+
     // Read the timer value
     for(int n = 0; n < 3; n++) {
       char c = ethernetClient.read();
@@ -125,7 +171,7 @@ void loop() {
       }
       values[1][n] = c;
     }
-    
+
     // Read each line of 'active user' data
     for(int i = 2; i < 10; i++) {
       for(int n = 0; n < 5; n++) {
@@ -140,10 +186,12 @@ void loop() {
         values[i][n] = c;
       }
     }
- 
+
     // Redraw the screen   
     cycleDisplay();
- 
+    setFlairColour();
+
   }
 
 }
+
